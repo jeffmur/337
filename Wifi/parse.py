6f832050bcb5584114277a84fbda0faf5570ff0a
Jeffrey Murray <jeffmur@uw.edu>
2019-05-09 01:17:32 -0700

    IDK

--- Wifi/parse.py
+++ Wifi/parse.py
@@ -3,13 +3,20 @@
 import numpy as np
 import pandas as pd
 
-df = pd.read_csv(filepath_or_buffer = "nexusOne.csv",
-				 names = ['Date', 'Protocol', 'Result'],
+reader = pd.read_csv(filepath_or_buffer = "/home/jeffmur/nexusOne.csv",
+				 names = ['Protocol', 'Result'],
 				 sep = ";",
-				 usecols = [2, 3, 4]);
+				 chunksize = 1,
+                                 iterator = True,
+				 keep_date_col = True,
+				 na_values=['N/A'],
+				 usecols = [3, 4]);
+for chunk in reader:
+     print(chunk)
 
-print(df)
-
+# Looking for 
+# 	"phone|sim|serial"
+#	"wifi|scan| MAC ADDRESS |ssid"
 # find "wifi" (constant number of characters until 1000, 10000, etc.)
 # then throw out the preceeding data 
 
