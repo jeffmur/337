9a42e63e5a474d3aa059b3127a2ec1623fc04e61
Jeffrey Murray <jeffmur@uw.edu>
2019-05-15 21:47:10 -0700

    done

--- Wifi/parse.py
+++ Wifi/parse.py
@@ -1,37 +1,53 @@
 #Time; nameofdevice; macaddress; SSID
-
+from timeit import default_timer as timer
 import numpy as np
 import pandas as pd
 #/home/jeffmur/nexusOne.csv
-reader = pd.read_csv(filepath_or_buffer = "smallData.csv",
+reader = pd.read_csv(filepath_or_buffer = "/home/jeffmur/nexusOne.csv",
 				 names = ['Garbage', 'Garbage2', 'time', 'protocol', 'result'],
 				 sep = ";",
 				 keep_date_col = True,
 				 na_values=['N/A']);
 
-df = pd.DataFrame(reader, columns=['protocol', 'result'])
+# set up dataFrame for protocol and result
+df = pd.DataFrame(reader, columns=['time', 'protocol', 'result'])
+
+# remove any duplicates
+df.drop_duplicates(keep='first')
+
 
 # found serial number
-def printSerialNumber():
+def getData():
+   start = timer()
+   data = []
    j = -1
    for i in df['protocol']:
       j+=1
+      # Assumption: name comes before data
       if(i == 'phone|sim|serial'):
-         print(df['result'][j])
-
-# found macaddress SHA
-def printMacSSID() :
-   j = -1
-   for i in df['protocol']:
-      j+=1
-      if((i[0:4] == 'wifi') & (i[(len(i)-1)] == 'd')):
-	 # macAddress
-	 print("macAddress: " + i[10:(len(i)-5)])
+	 name = df['result'][j]
+      # check for w and i 
+      if((i[0] == 'w') & (i[len(i)-2] == 'i')):
+	 # check for connected wifi
+	 if(i[5] == 'c'):
+	    macAddress = i[16:(len(i)-5)]
+	 else:
+	    # macAddress
+	    macAddress = i[10:(len(i)-5)]
 	 # ssid
-	 print("SSID: " + df['result'][j])
+         if(len(macAddress) <= 40):
+	    ssid = df['result'][j]
+	    time = df['time'][j]
+	    data.append({'time':time, 'serialNumber':name, 'macaddress':macAddress, 'ssid':ssid});
+    
+   parsedData = pd.DataFrame(data)
+   end = timer()
+   print("Time elapsed: " + str(end - start))
+   return parsedData
+	 
 
-printSerialNumber()
-printMacSSID()
+print(getData())
+
 
 
 # Looking for 
