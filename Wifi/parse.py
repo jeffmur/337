6769cb3da826b47a6b91da14a634416cd9ffc0b8
Jeffrey Murray <jeffmur@uw.edu>
2019-05-22 18:44:09 -0700

    FINIHSED

--- Wifi/parse.py
+++ Wifi/parse.py
@@ -1,9 +1,12 @@
-#Time; nameofdevice; macaddress; SSID
+import sys
 from timeit import default_timer as timer
 import numpy as np
 import pandas as pd
 #   /home/jeffmur/nexusOne.csv      smallData.csv
-reader = pd.read_csv(filepath_or_buffer = "/home/jeffmur/nexusOne.csv",
+
+dataFile = "smallData.csv"
+
+reader = pd.read_csv(filepath_or_buffer = dataFile,
 				 names = ['Garbage', 'Garbage2', 'time', 'protocol', 'result'],
 				 sep = ";",
 				 keep_date_col = True,
@@ -16,58 +19,37 @@ df = pd.DataFrame(reader, columns=['time', 'protocol', 'result'])
 # remove any duplicates
 df['result'].drop_duplicates(keep='first')
 
-
-# found serial number
-def getData():
-   start = timer()
-   data = []
-   j = -1
-   for i in df['protocol']:
-      j+=1
-      # Assumption: name comes before data
-      if(i == 'phone|sim|serial'):
-	 name = df['result'][j]
-      # check for w and i 
-      if((i[0] == 'w') & (i[len(i)-2] == 'i')):
-	 # check for connected wifi
-	 if(i[5] == 'c'):
-	    macAddress = i[16:(len(i)-5)]
-	 else:
-	    # macAddress
-	    macAddress = i[10:(len(i)-5)]
-	 # ssid
-         if(len(macAddress) <= 40):
-	    ssid = df['result'][j]
-	    time = df['time'][j]
-	    data.append({'time':time, 'serialNumber':name, 'macaddress':macAddress, 'ssid':ssid});
-    
-   parsedData = pd.DataFrame(data)
-   end = timer()
-   print("Time elapsed: " + str(end - start))
-   return parsedData
-
+# locates phone|sim|serial number
 def loc_serial(df):	
     name = df.iloc[np.where(df.protocol.values == 'phone|sim|serial')]
-    return name['result'].drop_duplicates(keep='first')
-
+    return name['result'].drop_duplicates(keep='first')       
+ 
+# mac, ssid, time to DataFrame
 def loc_mac(df):
-    start = timer()
     base = df.iloc[np.where(df.protocol.str.contains('ssid'))]
     time = base['time']
-    device = [loc_serial(df).values] * len(base)
+    for v in loc_serial(df).values: device = v
     ssid = base['result']
     mac = base['protocol']
     mac = mac.map(lambda x: str(x)[16:-5] if(str(x)[5] == 'c')	
                              else str(x)[10:-5])
     data = ({'time':time.values, 'macaddress':mac.values, 'ssid':ssid.values});
-    end = timer()
-    print("Time elapsed: " + str(end - start))
+    data['device'] = device
     return pd.DataFrame(data)
 
-print loc_mac(df)
-# Looking for 
+start = timer()
+data = loc_mac(df)
+data.to_csv(r'parsedData.csv',index=False, sep=";")
+end = timer()
+print("Time elapsed: " + str(end - start))
+
+print(data)
+
+# Found 
 # 	"phone|sim|serial"
 #	"wifi|scan| MAC ADDRESS |ssid"
-# find "wifi" (constant number of characters until 1000, 10000, etc.)
-# then throw out the preceeding data 
+#
+# Into DataFrame
+#	device; macaddress; SSID; Time
+
 
